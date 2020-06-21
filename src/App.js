dee4822f9ae1296a442802a1e51f919d7c254242
Usama Zahid <usamazahid100@gmail.com>
2020-06-21 22:55:32 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-   <div>Hello World</div>
+   <div>Hello World from Usama Zahid.</div>
   );
 }
 
